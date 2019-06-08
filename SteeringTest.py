9b4272bc9050f887836a38eb80271ac5ccac7c0f
Zeick <timo.j.karkkainen@helsinki.fi>
2019-06-08 23:06:46 +0300

    Toinen ohjelma

---
+++ SteeringTest.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button, Color, SoundFile, ImageFile, Align)
+from pybricks.tools import print, wait, StopWatch
+from pybricks.robotics import DriveBase
+from ev3dev2.motor import MoveSteering, OUTPUT_A, OUTPUT_B
+
+# Write your program here
+brick.sound.file(SoundFile.FANFARE)
+# Toinen idea steeringin kautta
+steering_pari = MoveSteering(OUTPUT_A,OUTPUT_B)
+steer_pair.on_for_rotations(steering=-20, speed=75, rotations=10)
+brick.sound.file(SoundFile.STOP)
+
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor
 from pybricks.parameters import (Port, Stop, Direction, Button, Color, SoundFile, ImageFile, Align)
 from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
-from ev3dev2.motor import MoveTank, OUTPUT_A, OUTPUT_B, MediumMotor
+from ev3dev2.motor import MoveTank, OUTPUT_A, OUTPUT_B
 
 # Write your program here
 brick.sound.file(SoundFile.HELLO)
