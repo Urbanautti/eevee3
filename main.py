b73fbfea7ddf0de335c6398911d9fa5ed6ab41e8
Zeick <timo.j.karkkainen@helsinki.fi>
2019-06-08 22:43:27 +0300

    Small change only

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env pybricks-micropython
 
 from pybricks import ev3brick as brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import (Port, Stop, Direction, Button, Color,
-                                 SoundFile, ImageFile, Align)
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button, Color, SoundFile, ImageFile, Align)
 from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 from ev3dev2.motor import MoveTank, OUTPUT_A, OUTPUT_B, MediumMotor
